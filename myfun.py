89d0c27c299de96495d0aec94acbffb4d3d077e9
LetiziaL00 <20033706@studenti.uniupo.it>
2025-03-11 13:59:49 +0100

    myfun.py

--- myfun.py
+++ myfun.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy import stats
+import matplotlib.pyplot as plt
 
 def normal(mean, std, color="black", num_points=200):
     x = np.linspace(mean-6*std, mean+6*std, num_points)
-    
+    p = stats.norm.pdf(x, mean, std)
+    plt.plot(x, p, color, linewidth=2)
